8d45676662bd25a298dc1fed650a5315e15ebf8c
kuvii <kuvilet@gmail.com>
2022-11-16 13:05:33 +0000

    Feat(main): Añadir cambios (fecha, textos)

--- validacion.py
+++ validacion.py
@@ -1,41 +1,41 @@
 import os, platform
 from multiprocessing import Process
 from time import sleep
+from datetime import datetime
 
 if (platform.system() == "Windows"):
     def hijo():
-        print("  > Son pid: ", os.getpid())
-        sleep(5)
-        print("Process ", os.getpid(), " ended")
+        print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
+        sleep(3)
+        print("Terminando el proceso con pid <", os.getpid(), ">")
         os._exit(0)
     
     def padre():
         print("Pid Windows, padre: ", os.getpid())
-        for i in range(processRepeat) :
+        for i in range(10) :
             p = Process(target=hijo)
             p.start()
+            sleep(10)
             p.join(0)
     
     if __name__ == '__main__' :
-        processRepeat = int(input("Cuantos procesos quieres abrir: "))
         padre()
 
 elif (platform.system() == "Linux"):
-    
     def padre():
         print("Pid padre: ", os.getpid())
-        for i in range(processRepeat):
+        for i in range(10):
             sonPid = os.fork()
+            sleep(10)
             if sonPid == 0:
                 hijo()
           
     
     def hijo():
-        print("  > Son pid: ", os.getpid())
-        sleep(5)
-        print("Process ", os.getpid(), " ended")
+        print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
+        sleep(3)
+        print("Terminando el proceso con pid <", os.getpid(), ">")
         os._exit(0)
         
     if __name__ == '__main__' :
-        processRepeat = int(input("Cuantos procesos quieres abrir: "))
         padre()
