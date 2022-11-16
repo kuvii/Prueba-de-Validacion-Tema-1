548c23a8246321eac7caa993c2da9ba4c8081110
kuvii <kuvilet@gmail.com>
2022-11-16 13:07:08 +0000

    CI(main): Añadir constantes

--- validacion.py
+++ validacion.py
@@ -3,19 +3,22 @@ from multiprocessing import Process
 from time import sleep
 from datetime import datetime
 
+PROCESS_WAIT_TIME = 10
+SON_WAIT_TIME = 3
+AMOUNT_PROCESSES = 10
 if (platform.system() == "Windows"):
     def hijo():
         print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
-        sleep(3)
+        sleep(SON_WAIT_TIME)
         print("Terminando el proceso con pid <", os.getpid(), ">")
         os._exit(0)
     
     def padre():
         print("Pid Windows, padre: ", os.getpid())
-        for i in range(10) :
+        for i in range(AMOUNT_PROCESSES) :
             p = Process(target=hijo)
             p.start()
-            sleep(10)
+            sleep(PROCESS_WAIT_TIME)
             p.join(0)
     
     if __name__ == '__main__' :
@@ -24,16 +27,16 @@ if (platform.system() == "Windows"):
 elif (platform.system() == "Linux"):
     def padre():
         print("Pid padre: ", os.getpid())
-        for i in range(10):
+        for i in range(AMOUNT_PROCESSES):
             sonPid = os.fork()
-            sleep(10)
+            sleep(PROCESS_WAIT_TIME)
             if sonPid == 0:
                 hijo()
           
     
     def hijo():
         print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
-        sleep(3)
+        sleep(SON_WAIT_TIME)
         print("Terminando el proceso con pid <", os.getpid(), ">")
         os._exit(0)
         
