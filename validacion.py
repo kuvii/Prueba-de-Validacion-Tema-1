6a90df7db7472e18fa23a543da05cdf8a2352dc2
kuvii <kuvilet@gmail.com>
2022-11-17 10:26:05 +0000

    Fix(main): Arreglar errores correccio

--- validacion.py
+++ validacion.py
@@ -6,39 +6,32 @@ from datetime import datetime
 PROCESS_WAIT_TIME = 10
 SON_WAIT_TIME = 3
 AMOUNT_PROCESSES = 10
-if (platform.system() == "Windows"):
-    def hijo():
-        print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
-        sleep(SON_WAIT_TIME)
-        print("Terminando el proceso con pid <", os.getpid(), ">")
-        os._exit(0)
-    
-    def padre():
-        print("Pid Windows, padre: ", os.getpid())
-        for i in range(AMOUNT_PROCESSES) :
-            p = Process(target=hijo)
-            p.start()
-            sleep(PROCESS_WAIT_TIME)
-            p.join(0)
-    
-    if __name__ == '__main__' :
-        padre()
 
-elif (platform.system() == "Linux"):
-    def padre():
-        print("Pid padre: ", os.getpid())
-        for i in range(AMOUNT_PROCESSES):
-            sonPid = os.fork()
-            sleep(PROCESS_WAIT_TIME)
-            if sonPid == 0:
-                hijo()
-          
-    
-    def hijo():
-        print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
-        sleep(SON_WAIT_TIME)
-        print("Terminando el proceso con pid <", os.getpid(), ">")
-        os._exit(0)
+def hijo():
+    print("Iniciando el proceso: <", os.getpid(), "> a las <", datetime.now().strftime("%H:%M:%S"), ">")
+    sleep(SON_WAIT_TIME)
+    print("Terminando el proceso con pid <", os.getpid(), ">")
+    os._exit(0)
+
+def padreWindows():
+    print("Pid Windows, padre: ", os.getpid())
+    for i in range(AMOUNT_PROCESSES) :
+        p = Process(target=hijo)
+        p.start()
+        p.join(0)
+        sleep(PROCESS_WAIT_TIME)
         
-    if __name__ == '__main__' :
-        padre()
+def padreUnix():
+    print("Pid padre: ", os.getpid())
+    for i in range(AMOUNT_PROCESSES):
+        sonPid = os.fork()
+        if sonPid == 0:
+            hijo()
+        else: 
+            sleep(PROCESS_WAIT_TIME)
+
+if __name__ == '__main__' :
+    if (platform.system() == "Windows"):
+        padreWindows()
+    else: 
+        padreUnix()
